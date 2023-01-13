eab203957a9923e7dac9458636f97d5b22fcc7be
Martin Quach <martin_quach@hotmail.com>
2023-01-13 11:41:23 +0100

    Obstacles with updated base spawner
    Added obstacles to the field, the base spawner will not spawn inside any obstacles and can spawn anywhere on the field.

--- VRSpoolGun/Assets/BaseSpawner.cs
+++ VRSpoolGun/Assets/BaseSpawner.cs
@@ -10,6 +10,7 @@ public class BaseSpawner : MonoBehaviour
     private bool canSpawn;
     private int bases = 0;
 
+    [SerializeField] LayerMask spawnLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,7 +36,23 @@ public class BaseSpawner : MonoBehaviour
 
     void SpawnBase()
     {
-        GameObject newBase = Instantiate(eBase, new Vector3(Random.Range(3,7),0,Random.Range(3,7)), Quaternion.identity);
-        newBase.GetComponent<EnemyBase>().creator = this; 
+        Vector3 minimumRange = new Vector3(9.3f, 0, 9.3f);
+        Vector3 maximumRange = new Vector3(10, 0, 10);
+        Vector3 testBoxPosition = new Vector3(4.1f, 0, 7.3f);
+
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 boxPosition = new Vector3(Random.Range(9.3f, 9.3f), Random.Range(0, 0), Random.Range(9.3f, 9.3f));
+
+            if (!Physics.CheckBox(boxPosition, eBase.transform.localScale, Quaternion.identity, spawnLayer))
+            {
+                GameObject newBase = Instantiate(eBase, boxPosition, Quaternion.identity);
+                newBase.GetComponent<EnemyBase>().creator = this;
+                break;
+            }
+          Debug.Log("Target hit.");
+        }
+
+        Debug.Log("YES!");
     }
 }
